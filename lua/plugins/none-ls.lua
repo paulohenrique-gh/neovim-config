b9b00a206f86f1616185ccef2fd477aaeb9b2722
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-03-16 22:57:29 -0300

    fix eslint_d

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,20 +1,25 @@
 -- generalized lsp, for things like rubopcop which isn't an LSP but with null_ls it acts like one (??)
 -- stylua needs to be installed with :Mason
+-- issue with eslint_d fixed according to stack overflow solution: https://stackoverflow.com/questions/78150730/failed-to-load-builtin-eslint-d-for-method-diagnostics
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
+    null_ls.setup({
+      sources = {
+        require("none-ls.diagnostics.eslint_d"),
+        null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.erb_lint,
         null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.rubocop,
-			},
-		})
+      },
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
