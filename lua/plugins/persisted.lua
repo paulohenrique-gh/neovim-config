977284e16e8864783bea1222aa7092039c564c39
Paulo Henrique de Meneses Oliveira <ph.1928@hotmail.com>
2024-06-25 09:44:42 -0300

    Disable persisted.nvim

--- lua/plugins/persisted.lua
+++ lua/plugins/persisted.lua
@@ -1,5 +1,5 @@
 return {
-	"olimorris/persisted.nvim",
-	lazy = true, -- make sure the plugin is always loaded at startup
-	config = true,
+	-- "olimorris/persisted.nvim",
+	-- lazy = true, -- make sure the plugin is always loaded at startup
+	-- config = true,
 }
